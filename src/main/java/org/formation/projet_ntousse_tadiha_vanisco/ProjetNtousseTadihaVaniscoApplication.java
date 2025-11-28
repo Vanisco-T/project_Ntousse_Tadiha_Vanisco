d5dc46312eaa4825d1106682bd353fd8937546e6
Vanisco NTOUSSE TADIHA <vanisco.ntousse-tadiha@epita.fr>
2025-11-28 09:43:33 +0100

    project initialisation

---
+++ src/main/java/org/formation/projet_ntousse_tadiha_vanisco/ProjetNtousseTadihaVaniscoApplication.java
@@ -0,0 +1,13 @@
+package org.formation.projet_ntousse_tadiha_vanisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetNtousseTadihaVaniscoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetNtousseTadihaVaniscoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/formation/projet_ntousse_tadiha_vanisco/ProjetNtousseTadihaVaniscoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.projet_ntousse_tadiha_vanisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetNtousseTadihaVaniscoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
